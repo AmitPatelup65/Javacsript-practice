81b9a5b17b9f87c6564ed2c794b7defa3dfb7256
AmitPatelup65 <patelboy330@gmail.com>
2025-09-01 21:39:26 +0530

    practice in java script

---
+++ practice.js
@@ -0,0 +1,20 @@
+let arr=["Amit","Patel"]
+// arr.push("Mango")
+// arr.unshift("Pineapple")
+// console.log(arr);
+// let narr=arr.map((item)=>item*item)
+// console.log(narr)
+// let n=arr.filter((item)=>item>10)
+// console.log(n)
+// let n=arr.reduce((acc,val)=>{
+//     return acc+val
+// },0)
+
+//  console.log(n)
+// let n=arr.some((item)=>item<35)
+// console.log(n)
+// let [a,b]=arr
+// console.log(a)
+// console.log(b)
+// arr =["hello",...arr]
+// console.log(arr)
--- reverse.js
+++ reverse.js
@@ -1,2 +1,24 @@
 let arr=[1,2,3,4,5]
-console.log(arr.reverse());
+// arr.forEach((val,i,c)=>{
+//     console.log(` value-${val} index- ${i} ${c}`);
+    
+// })
+// let n=arr.map((item)=>{
+//  if(item>1)
+//     return item
+// })
+ 
+// console.log(n);
+// let a=arr.reduce((acc,val)=>{
+//     return acc*val
+// },1)
+// console.log(a);
+ let sum=0
+arr.forEach(item=>{
+    return sum=sum+item
+})
+console.log(sum);
+
+
+
+ 
