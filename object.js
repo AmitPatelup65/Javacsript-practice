c43fb19b2e5c2cd63979101d79a27d99235016da
AmitPatelup65 <patelboy330@gmail.com>
2025-09-03 21:53:08 +0530

    update

--- object.js
+++ object.js
@@ -1,16 +1,22 @@
 let a={
     name:"Amit",
     lname:"Patel",
-    detail:{
-        age:21,
-        roll:22
+    addres:{
+        city:"Varansi"
     }
-}
-let {age,roll}=a.detail
- console.log(a.name,a.lname)
- console.log(a.detail.age,a.detail.roll)
- console.log(age,roll)
- for(let key in a){
-    console.log(key,a[key])
- }
- console.log(Object.keys(a))
+    
+    }
+
+// let {age,roll}=a.detail
+//  console.log(a.name,a.lname)
+//  console.log(a.detail.age,a.detail.roll)
+//  console.log(age,roll)
+//  for(let key in a){
+//     console.log(key,a[key])
+//  }
+//  console.log(Object.keys(a))
+// let obj1={...a}
+let obj1=JSON.parse(JSON.stringify(a))
+obj1.addres.city="Mirzapur"
+console.log(a)
+console.log(obj1)
