f603f82c0fad8b900661f66bc5e9dc5130d7e2df
AmitPatelup65 <patelboy330@gmail.com>
2025-09-29 22:46:39 +0530

    update

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-// let id=document.getElementById('ab')
-let a=document.getElementsByClassName('abc')
-console.log(a)
-const b="hi"
-const name=new String("patel")
+function add (a,b){
+    return a+b;
+}
+console.log(add(2,4))
+
 
--- input.js
+++
@@ -1,6 +0,0 @@
-const readline= require('readline-sync');
-// let n=readline.questionInt('Enter The NUmber')
-// let x=readline.questionInt('Enter The NUmber')
-let inputs = readline.question('Enter two numbers separated by space: ');
-let [n, x] = inputs.split(' ').map(Number);
-console.log(n+x)
