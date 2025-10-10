0da248935f7cbf5b9607353ba215727c998ed23a
AmitPatelup65 <patelboy330@gmail.com>
2025-10-10 21:11:47 +0530

    update

--- hello.js
+++ hello.js
@@ -1,12 +1,19 @@
-function add (a,b){
-    return a+b;
-}
-function sub (a,b){
-    return a-b;
-}
-console.log(add(2,4))
-console.log(add(2,4))
-console.log(add(2,4))
-console.log(sub(2,4))
+// const express= require("express");
+// const app=express();
+// const port=3000;
+// app.use(express.json());
+// app.get("/",(req,res)=>{
+//   res.send("Server Is working")
+// })
+// app.get("/me/:id",(req,res)=>{
+//     res.send(`The id Is :${req.params.id}`)
+// })
+// app.post("/me",(req,res)=>{
+//     const {email,password}=req.body
+//     res.send(`The Email Is ${email} The password Is ${password}`)
+// })
+// app.listen(port,()=>{
+//     console.log(`Server Is Runnnig At http://localhost:${port}`)
+// })
 
 
---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const app =express();
+const port=4000;
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send(`khul gya bhai`)
+})
+app.post('/home',(req,res)=>{
+    const {email,password,name,surname}=req.body;
+    res.send(`The Email Is ${email} Pasword Is ${password} my name is ${name}`)
+})
+app.listen(port,()=>{
+  console.log(`Server Is Runnig on : http://localhost:${port}`)
+})
+app.post('/about',(req,res)=>{
+    const {name,age}=req.body
+    res.send(`Name Is ${name} age is ${age}`)
+})
